ee3e85ed73f063b8ad3aea7b7d992a981dc4e2a1
chisl <info@chisl.io>
2018-01-23 05:33:14 +0100

    update

--- DS1307.h
+++ DS1307.h
@@ -2,8 +2,14 @@
  * name:        DS1307
  * description: Lowpower serial real-time clock (RTC) with full binary-coded decimal (BCD) clock/calendar plus 56 bytes of NV SRAM.
  * manuf:       Maxim Integrated
- * version:     0.1
+ * version:     Version 0.1
  * url:         https://datasheets.maximintegrated.com/en/ds/DS1307.pdf
  * date:        2017-01-11
  */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
 
